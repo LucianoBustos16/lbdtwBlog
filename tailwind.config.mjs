588e8c13a89630fac0869af1cc5c7f3a7ecce905
Luciano Bustos <bustosluciano89@gmail.com>
2024-08-13 22:09:57 -0300

    Agregar libreria de animaciones y plugins

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+import animations from '@midudev/tailwind-animations'
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary': '#0598D1',
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		animations
+	],
 }
